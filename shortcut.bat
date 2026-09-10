a036eaf66130fb0633f0165b7c5c0767b95bebf7
Naxomega <124938856+Naxomega@users.noreply.github.com>
2026-09-10 19:25:13 +0200

    final hotfix

--- shortcut.bat
+++ shortcut.bat
@@ -1,10 +1,10 @@
 @echo off
 set EXEPATH=%1
 set /p NAME=Please input the shortcut name. (without .lnk) 
-echo Set oWS = WScript.CreateObject("WScript.Shell") > CreateShortcut.vbs
-echo sLinkFile = "%appdata%\Microsoft\Windows\Start Menu\Programs\%NAME%.lnk" >> CreateShortcut.vbs
-echo Set oLink = oWS.CreateShortcut(sLinkFile) >> CreateShortcut.vbs
-echo oLink.TargetPath = %EXEPATH% >> CreateShortcut.vbs
-echo oLink.Save >> C:/CreateShortcut.vbs
-cscript C:/CreateShortcut.vbs
-del C:/CreateShortcut.vbs
+echo Set oWS = WScript.CreateObject("WScript.Shell") > %localappdata%\CreateShortcut.vbs
+echo sLinkFile = "%appdata%\Microsoft\Windows\Start Menu\Programs\%NAME%.lnk" >> %localappdata%\CreateShortcut.vbs
+echo Set oLink = oWS.CreateShortcut(sLinkFile) >> %localappdata%\CreateShortcut.vbs
+echo oLink.TargetPath = %EXEPATH% >> %localappdata%\CreateShortcut.vbs
+echo oLink.Save >> %localappdata%\CreateShortcut.vbs
+start "" %localappdata%\CreateShortcut.vbs
+echo Done.
